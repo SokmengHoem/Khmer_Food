a540d1291411068b1b70405b47fd7ad68d61fae5
Sokmeng Hoem <sokmeng168msi@gmail.com>
2024-06-05 18:02:08 +0700

    Change color

--- src/components/Organisms/layouts/NavLink.tsx
+++ src/components/Organisms/layouts/NavLink.tsx
@@ -9,7 +9,7 @@ export default function
     <div className=' w-full h-auto'>
         <div className=' w-[82%] bg-[#D9D9D9] rounded-xl  mx-auto flex items-center'>
             <div>
-                <Link to="" className=' text-blue-600 text-lg font-bold'>Show All</Link>
+                <Link to="" className=' text-blue-600 text-lg '>Show All</Link>
                 <Link to="" className=' '>Khmer food</Link>
                 <Link to="" className=' '>Num Khmer</Link>
                 <Link to="" className=' '>Drink</Link>
