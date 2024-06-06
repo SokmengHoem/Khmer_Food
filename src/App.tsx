8d7b75bc4907696deed743603d7803b6fc228a98
easycode002 <easycode002@gmail.com>
2024-06-06 22:42:34 +0700

    Page not found

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import AboutPage from './pages/about/AboutPage'
 import Layout from './components/Organisms/layouts/Layout'
 import ContactPage from './pages/contact/ContactPage'
 import FoodPage from './pages/food/FoodPage'
-import FoodDetails from './pages/food/FoodDetails'
 import FavoritePage from './pages/fav/FavoritePage'
 import AddToCard from './pages/addCard/AddToCard'
 import LoginPage from './pages/auth/LoginPage'
 import Register from './pages/auth/Register'
 import FAQPage from './pages/faq/FAQPage'
 import BlogPage from './pages/blog/BlogPage'
+import ErrorPage from './pages/error/404_error'
 
 function App() {
  
@@ -21,7 +21,7 @@ function App() {
          <Route path="/" element={<Layout/>}>
           <Route path="" element={<HomePage />} />
           <Route path="food" element={<FoodPage/>}/>
-          <Route path="food-details" element={<FoodDetails />} />
+          {/* <Route path="food-details" element={<FoodDetails />} /> */}
           <Route path="about" element={<AboutPage />} />
           <Route path="contact" element={<ContactPage/>} />
           <Route path="favorite" element={<FavoritePage/>}/>
@@ -30,7 +30,7 @@ function App() {
           <Route path="blog" element={<BlogPage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<Register />} />
-          <Route path="food-detail" element={<FoodDetails />} />
+          <Route path="*" element={<ErrorPage/>}/>
          </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/error/404_error.tsx
@@ -0,0 +1,9 @@
+const _Errorr_404 = () =>{
+    return (
+        <div>
+            <h1>404 - Not Found</h1>
+        </div>
+    );
+}
+
+export default _Errorr_404;
